59fd4b5835e18fa8a0cd3adacdefdc8bdd6cc7c3
Mike Whiting <mail@mikejw.co.uk>
2016-05-16 11:37:04 +0100

    adding class gen.

---
+++ src/Empathy/ELib/Gen/Admin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Empathy\ELib\Gen;
+
+class Admin extends \Empathy\MVC\Util\ControllerGen
+{
+    protected $name = 'dsection';
+    protected $module = 'admin';
+    protected $parent = '\Empathy\ELib\DSection\Controller';
+}
+
